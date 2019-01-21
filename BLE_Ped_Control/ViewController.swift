7236b298319ea1671b13a40d191d4d5731d3f855
Larry Bonnette <larrybonnette@Larrys-Mac.local>
2019-01-21 06:14:20 -0600

    Fixing

--- BLE_Ped_Control/ViewController.swift
+++ BLE_Ped_Control/ViewController.swift
@@ -1,9 +1,10 @@
 //
 //  ViewController.swift
-//  BLE_Ped_Control
+//  Toast
 //
-//  Created by Larry Bonnette on 1/20/19.
-//  Copyright © 2019 Larry Bonnette. All rights reserved.
+//  Created by Bear Cahill on 2/13/18.
+//  Copyright © 2018 Bear Cahill. All rights reserved.
+//  Modified by Larry Bonnette (12/2018) to control an led on an ESP32 (Arduino "like" device)
 //
 
 import UIKit
@@ -15,9 +16,10 @@ let arduinoLEDstate = CBUUID.init(string: "DF03") // This is the Tx charateristi
 var LedSendChar: CBCharacteristic! // This is the data sent to the Arduino (for the LED on/off)
 var LedReadState: CBCharacteristic! // This is the data sent to the iPhone indicating the LED on or off state
 var savedPeripheral: CBPeripheral? // This is the peripheral name used to address the Adruino
+var led = false // This holds the recieved (from the Arduino) led state (on or off)
 
-class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate  {
-
+// This Viewcontroller controls the "View" as well as the "Blootooth" services
+class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
     @IBAction func Stop(_ sender: Any) {
     }
     @IBAction func Up(_ sender: Any) {
@@ -28,6 +30,9 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
     }
     @IBAction func Left(_ sender: Any) {
     }
+    
+
+    
     // This looks to see if Bluetooth is powered on
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         if central.state == CBManagerState.poweredOn {
@@ -36,6 +41,7 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
             print ("scanning...") // sent to console for debug purposes
         }
     }
+    
     // If we find a periphal we look to see if it is our ESP32
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
         if peripheral.name?.contains("ESP") == true { // if it's ours it will be advertising "ESP"
@@ -86,11 +92,55 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
         // We are connected to toaster and we have all the info needed to begin so....
 
     }
+    
+    // If we get a notification of "Data"
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        // We check to see if it is our data that is ready for us to process
+        if characteristic.uuid == arduinoLEDstate {
+            let s = characteristic.value![0] // Get the ascii value of the value from the BLE device
+            let pz = Character(UnicodeScalar(s)) // Convert ascii to a charater
+            print ("Recieved value '\(pz)' from the ESP32")
+            if pz == "N" { // If the Arduino is sending us an "N" it means that the LED is "on"
+                led = true
+                print("LED is on") // sent to console for debug purposes
+
+                
+                
+            }
+            if pz == "X"{ // If the Arduino is sendig us an "X" it means that the LED is "off"
+                led = false
+                print("LED is off") // sent to console for debug purposes
+
+            }
+            if pz == "R"{ // If the Arduino is sendig us an "R" it means that the toaster is "Ready"
+                led = false
+                print("Toaster is ready") // sent to console for debug purposes
+
+                
+            }
+        }
+        
+    }
+    // We can do something when we write a value to the Arduino. In this case we just print to the console.
+    func peripheral(_ peripheral: CBPeripheral, didWriteValueFor characteristic: CBCharacteristic, error: Error?) {
+        print ("wrote value to ESP32") // sent to console for debug purposes
+    }
+    
+    var centralManager : CBCentralManager!
+    var myPeripheral : CBPeripheral?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        centralManager = CBCentralManager.init(delegate: self, queue: nil) // initialize the Blootooth Central Manager
+        
     }
-
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
 }
 
